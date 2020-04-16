17e93289dadba180620a564516fb72004724aef7
Jason Weirather <jason-weirather@users.noreply.github.com>
2020-04-16 11:21:02 -0400

    add helper script for getting database files

---
+++ data/db/download.sh
@@ -0,0 +1,4 @@
+wget http://kronos.pharmacology.dal.ca/public_files/picrust/picrust_precalculated_v1.1.4/13_5/16S_13_5_precalculated.tab.gz
+wget http://kronos.pharmacology.dal.ca/public_files/picrust/picrust_precalculated_v1.1.4/13_5/ko_13_5_precalculated.tab.gz
+wget ftp://greengenes.microbio.me/greengenes_release/gg_13_8_otus/rep_set/97_otus.fasta
+wget ftp://greengenes.microbio.me/greengenes_release/gg_13_8_otus/taxonomy/97_otu_taxonomy.txt
